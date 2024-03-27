d000c0a7f4b1956f795b6e75486f55a5c00bed3b
nayeem-miah <nayeem5113a@gmail.com>
2024-03-27 13:10:27 +0600

    icon

--- src/Components/ListedShowBook.jsx
+++ src/Components/ListedShowBook.jsx
@@ -1,6 +1,8 @@
-
+import { IoLocationOutline } from "react-icons/io5";
+import { IoIosContacts } from "react-icons/io";
+import { MdRestorePage } from "react-icons/md";
 const ListedShowBook = ({book}) => {
-    const {  author, rating, bookName, tags, image, category, totalPages,publisher } = book;
+    const {  author, rating, bookName, tags, image, category, totalPages,publisher,yearOfPublishing } = book;
     return (
         <div className="card  card-side bg-base-100 shadow-xl border mb-10">
   <figure className=""><img src={image} alt=""/></figure>
@@ -19,11 +21,11 @@ const ListedShowBook = ({book}) => {
               </button>
               
             ))}
-            <p>location none </p>
+            <p className="flex items-center"><IoLocationOutline className="text-2xl"></IoLocationOutline> Year of Publishing: {yearOfPublishing} </p>
           </div>
           <div className="flex justify-start border-b-2">
-          <p>{publisher}</p>
-          <p> pages: {totalPages}</p>
+          <p className="flex items-center gap-2"><IoIosContacts></IoIosContacts> {publisher}</p>
+          <p className="flex items-center gap-2"><MdRestorePage></MdRestorePage> pages: {totalPages}</p>
           </div>
     <div className="card-actions flex gap-4 ">
       <button className=" p-2 bg-blue-200 rounded-2xl border">category: {category}</button>
